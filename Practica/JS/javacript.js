75164242208815341f15ae41b89331934d2632b6
JoseConsultores98 <jose@consultoresweb.com.mx>
2023-05-17 19:01:36 -0500

    Inicio de JS

---
+++ Practica/JS/javacript.js
@@ -0,0 +1,6 @@
+function inicio() {
+    alert("Hola Mundo!");
+    alert("soy el primer script");
+    document.getElementById("demo").innerHTML = "Hola Mundo";
+    let age = prompt('¿Cuanto años tienes?', 100);
+}
